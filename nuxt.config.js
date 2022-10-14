e3a0886879a7fc436e53b86146b0c9acf0dc87ba
zelo0 <minorSunbi@gmail.com>
2022-10-14 12:02:01 +0900

    node-sasss -> sass

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ["@nuxtjs/eslint-module"],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
